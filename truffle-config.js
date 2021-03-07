ee854861ba0532bab7bca4dee87fae87986ee63a
kripsonud <kripsonud@gmail.com>
2021-03-07 20:38:38 +0100

    Fix issue with gas limit

--- truffle-config.js
+++ truffle-config.js
@@ -46,7 +46,7 @@ module.exports = {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
-     gas: 4612388
+     gas: 8721974
     },
     // Another network with more advanced options...
     // advanced: {
